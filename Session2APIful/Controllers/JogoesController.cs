d8130f934278c89ba59f69f95d46f3e3dc51e486
juandiegows <mejiamaestrejuandiego@gmail.com>
2022-09-08 18:00:12 -0500

    add controller logo and selecaos

---
+++ Session2APIful/Controllers/JogoesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Session2APIful.Models;
+
+namespace Session2APIful.Controllers
+{
+    public class JogoesController : ApiController
+    {
+        private Sessao2Entities db = new Sessao2Entities();
+
+        // GET: api/Jogoes
+        public IHttpActionResult GetJogo()
+        {
+            return Ok(db.Jogo.ToList().Select(x => new
+            {
+                x.Id,
+                x.Data,
+                Estadio = x.Estadio1.Nome,
+                Home = x.Selecao.Nome,
+                Visit = x.Selecao1.Nome,
+                HomeB = x.Selecao.Bandeira,
+                VisitB = x.Selecao1.Bandeira,
+                GoalH = x.PlacarCasa,
+                GoalV = x.PlacarVisitante   
+
+            }).ToList());
+        }
+
+        // GET: api/Jogoes/5
+        [ResponseType(typeof(Jogo))]
+        public IHttpActionResult GetJogo(int id)
+        {
+            var jogo = db.Jogo.ToList().Select(x => new
+            {
+                x.Id,
+                x.Data,
+                Estadio = x.Estadio1.Nome,
+                Home = x.Selecao.Nome,
+                Visit = x.Selecao1.Nome,
+                HomeB = x.Selecao.Bandeira,
+                VisitB = x.Selecao1.Bandeira,
+                GoalH = x.PlacarCasa,
+                GoalV = x.PlacarVisitante
+
+            }).FirstOrDefault(x => x.Id == id);
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(jogo);
+        }
+
+        // PUT: api/Jogoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutJogo(int id, Jogo jogo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != jogo.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(jogo).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JogoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Jogoes
+        [ResponseType(typeof(Jogo))]
+        public IHttpActionResult PostJogo(Jogo jogo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Jogo.Add(jogo);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (JogoExists(jogo.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = jogo.Id }, jogo);
+        }
+
+        // DELETE: api/Jogoes/5
+        [ResponseType(typeof(Jogo))]
+        public IHttpActionResult DeleteJogo(int id)
+        {
+            Jogo jogo = db.Jogo.Find(id);
+            if (jogo == null)
+            {
+                return NotFound();
+            }
+
+            db.Jogo.Remove(jogo);
+            db.SaveChanges();
+
+            return Ok(jogo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool JogoExists(int id)
+        {
+            return db.Jogo.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ Session2APIful/Controllers/SelecaosController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Session2APIful.Models;
+
+namespace Session2APIful.Controllers
+{
+    public class SelecaosController : ApiController
+    {
+        private Sessao2Entities db = new Sessao2Entities();
+
+        // GET: api/Selecaos
+        public IHttpActionResult GetSelecao()
+        {
+            return Ok(db.Selecao.ToList().Select(x => new
+            {
+                x.Id,
+                x.Bandeira,
+                x.Nome,
+                Point = Point(x.Id)
+
+            }).ToList().OrderBy(x=> x.Nome).ThenBy(x=> x.Point).ToList());
+        }
+
+        public int Point(int id)
+        {
+            var seleccion = db.Selecao.Find(id);
+            int cant = seleccion.Jogo.Count(x => x.PlacarVisitante == x.PlacarCasa);
+            cant += seleccion.Jogo1.Count(x => x.PlacarVisitante == x.PlacarCasa);
+            cant += seleccion.Jogo.Count(x => x.PlacarCasa > x.PlacarVisitante) * 3;
+            cant += seleccion.Jogo1.Count(x => x.PlacarCasa < x.PlacarVisitante) * 3;
+
+                return cant;
+        }
+
+        // GET: api/Selecaos/5
+        public IHttpActionResult GetSelecao(int id)
+        {
+            var selecao = db.Selecao.ToList().Select(x => new
+            {
+                x.Id,
+                x.Bandeira,
+                x.Nome,
+                Point = Point(x.Id)
+
+            }).ToList().OrderBy(x => x.Nome).ThenBy(x => x.Point).FirstOrDefault(x=> x.Id == id);
+            if (selecao == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(selecao);
+        }
+
+        // PUT: api/Selecaos/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSelecao(int id, Selecao selecao)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != selecao.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(selecao).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SelecaoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Selecaos
+        [ResponseType(typeof(Selecao))]
+        public IHttpActionResult PostSelecao(Selecao selecao)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Selecao.Add(selecao);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = selecao.Id }, selecao);
+        }
+
+        // DELETE: api/Selecaos/5
+        [ResponseType(typeof(Selecao))]
+        public IHttpActionResult DeleteSelecao(int id)
+        {
+            Selecao selecao = db.Selecao.Find(id);
+            if (selecao == null)
+            {
+                return NotFound();
+            }
+
+            db.Selecao.Remove(selecao);
+            db.SaveChanges();
+
+            return Ok(selecao);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SelecaoExists(int id)
+        {
+            return db.Selecao.Count(e => e.Id == id) > 0;
+        }
+    }
+}
